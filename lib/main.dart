22d9695108f325d33a0d676ec8b5b883f8823e9c
Deepanshu1924 <deepanshu19choudhary@gmail.com>
2024-11-06 11:17:31 +0530

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:admin_panel/LoginPage.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+Future<void> main() async {
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
 
   // This widget is the root of your application.
   @override
